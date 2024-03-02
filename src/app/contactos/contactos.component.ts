ec1e382af79242b3b571aa72a9b26dc72403e37a
David Burbano <davidrpy@gmail.com>
2024-03-01 21:01:12 -0500

    implementando contactos

--- src/app/contactos/contactos.component.ts
+++ src/app/contactos/contactos.component.ts
@@ -8,5 +8,10 @@ import { Component } from '@angular/core';
   styleUrl: './contactos.component.css'
 })
 export class ContactosComponent {
-
+  //para abrir directamente comunicacion con whatsapp
+  abrirWhatsApp(): void {
+    const numeroTelefono = '3135249496'; // Reemplaza con tu número de teléfono
+    const urlWhatsApp = `https://wa.me/${3135249496}`;
+    window.open(urlWhatsApp, '_blank');
+  }
 }
